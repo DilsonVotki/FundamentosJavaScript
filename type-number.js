e2ea433aed77031483b7ffd72b8f089e0e0c90bf
Dilson Votki <dilsonpl@gmail.com>
2022-11-19 09:43:57 -0300

    tipo number

---
+++ type-number.js
@@ -0,0 +1,35 @@
+const numero = 3;
+
+const primeiroNum = 2;
+const segundoNum = 4;
+
+const soma = primeiroNum + segundoNum;
+
+console.log("SOMA: " + soma);
+
+const menos = primeiroNum - segundoNum;
+
+console.log("SUBTRAÇÃO: " + menos);
+
+const divisao = primeiroNum / segundoNum;
+
+console.log("DIVISAO: " + divisao);
+
+const multi = primeiroNum * segundoNum;
+
+console.log("MULTIPLICAÇÃO: " + multi);
+
+//PONTO FLUTUANTE
+
+const numeroPtoFlutuante = 3.3;
+const numeroSemZero = 0.5;
+
+const novaOperacao = numeroPtoFlutuante / numeroSemZero;
+
+console.log("RESULTADO PONTO FLUTUANTE: " + novaOperacao);
+
+// NaN -> Not a number (não é Numero)
+
+const alura = "Alura";
+
+console.log("NaN: " + alura * novaOperacao);
